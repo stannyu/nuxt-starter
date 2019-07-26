f587a564dd7c46929c6ec1a826f19366b8a03317
stanislauy <stas.yudzitski@yandex.ru>
2019-07-26 15:44:34 +0300

    New layout added. User page styled & data fetched.

--- pages/team/index.vue
+++ pages/team/index.vue
@@ -5,9 +5,12 @@
     </div>
     <h2>Team index page.</h2>
 
-    <ul>
-      <li v-for="user in usersData" :key="user.id">
-        <nuxt-link :to="/team/ + user.id">User {{ user.id }}</nuxt-link>
+    <ul class="user_cards_container">
+      <li v-for="user in usersData" :key="user.id" class="user_card">
+        <nuxt-link :to="/team/ + user.id">
+          <img class="photo_wrapper" :src="user.userpic" alt="" />
+          <p>{{ user.username }}</p>
+        </nuxt-link>
       </li>
     </ul>
   </div>
@@ -20,19 +23,27 @@ export default {
       usersData: [
         {
           username: "John Doe",
-          id: 1
+          id: 1,
+          userpic:
+            "https://theinspirationgrid.com/app/uploads/2019/07/illustration-oleg-smirnov-08.jpg"
         },
         {
           username: "John Doe1",
-          id: 2
+          id: 2,
+          userpic:
+            "https://theinspirationgrid.com/app/uploads/2019/07/illustration-oleg-smirnov-11.jpg"
         },
         {
           username: "John Doe2",
-          id: 3
+          id: 3,
+          userpic:
+            "https://theinspirationgrid.com/app/uploads/2019/07/illustration-oleg-smirnov-05.jpg"
         },
         {
           username: "John Doe4",
-          id: 4
+          id: 4,
+          userpic:
+            "https://theinspirationgrid.com/app/uploads/2019/07/illustration-oleg-smirnov-03.jpg"
         }
       ]
     };
@@ -61,7 +72,27 @@ export default {
     margin-bottom: 100px;
   }
 }
-ul {
+.user_cards_container {
   list-style: none;
+  .user_card {
+    position: relative;
+    overflow: hidden;
+    width: 200px;
+    height: 300px;
+    border-radius: 5px;
+    border: 1px solid lightgray;
+    margin-bottom: 20px;
+    margin-right: 20px;
+    display: inline-block;
+    .photo_wrapper {
+      width: 100%;
+    }
+    p {
+      position: absolute;
+      bottom: 20px;
+      text-align: center;
+      width: 100%;
+    }
+  }
 }
 </style>
